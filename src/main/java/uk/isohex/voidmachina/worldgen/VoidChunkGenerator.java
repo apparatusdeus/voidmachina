35fe1ef3e8a84ede56c9e9b9e59552706e1190ae
James Oates <joates@pragmacharge.com>
2025-04-25 17:44:29 +0100

    Working

--- src/main/java/uk/isohex/voidmachina/worldgen/VoidChunkGenerator.java
+++ src/main/java/uk/isohex/voidmachina/worldgen/VoidChunkGenerator.java
@@ -1,111 +1,56 @@
-// package uk.isohex.voidmachina.worldgen;
+// package com.voidmachina.worldgen;
 
-// import java.util.List;
-// import java.util.concurrent.CompletableFuture;
-// import java.util.function.Function;
-
-// import com.mojang.serialization.MapCodec;
-
-// import net.minecraft.core.BlockPos;
 // import net.minecraft.core.Holder;
-// import net.minecraft.resources.ResourceKey;
+// import net.minecraft.core.RegistryAccess;
+// import net.minecraft.core.registries.Registries;
+// import net.minecraft.resources.ResourceLocation;
 // import net.minecraft.server.level.WorldGenRegion;
-// import net.minecraft.world.level.LevelHeightAccessor;
-// import net.minecraft.world.level.NoiseColumn;
 // import net.minecraft.world.level.StructureManager;
-// import net.minecraft.world.level.biome.Biome;
-// import net.minecraft.world.level.biome.BiomeGenerationSettings;
 // import net.minecraft.world.level.biome.BiomeManager;
 // import net.minecraft.world.level.biome.BiomeSource;
 // import net.minecraft.world.level.chunk.*;
 // import net.minecraft.world.level.levelgen.*;
-// import net.minecraft.world.level.levelgen.GenerationStep.Carving;
-// import net.minecraft.world.level.levelgen.Heightmap.Types;
-// import net.minecraft.world.level.levelgen.blending.Blender;
 // import net.minecraft.world.level.levelgen.structure.StructureSet;
+// import
+// net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 // public class VoidChunkGenerator extends ChunkGenerator {
 
-// public VoidChunkGenerator(BiomeSource pBiomeSource,
-// Function<Holder<Biome>, BiomeGenerationSettings> pGenerationSettingsGetter) {
-// super(pBiomeSource, pGenerationSettingsGetter);
+// public VEVoidChunkGenerator(Holder<StructureSet> structureSet, BiomeSource
+// biomeSource) {
+// super(structureSet, biomeSource);
 // }
 
 // @Override
-// protected MapCodec<? extends ChunkGenerator> codec() {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method 'codec'");
+// public void buildSurface(WorldGenRegion region, StructureManager
+// structureManager, RandomState randomState,
+// ChunkAccess chunk) {
+// // Intentionally left blank to create a void world
 // }
 
 // @Override
-// public void applyCarvers(WorldGenRegion pLevel, long pSeed, RandomState
-// pRandom, BiomeManager pBiomeManager,
-// StructureManager pStructureManager, ChunkAccess pChunk, Carving pStep) {
-// // Empty on purpose
+// public void applyCarvers(WorldGenRegion region, long seed, RandomState
+// randomState, BiomeManager biomeManager,
+// StructureManager structureManager, ChunkAccess chunk, GenerationStep.Carving
+// step) {
+// // No carvers applied in a void world
 // }
 
 // @Override
-// public void buildSurface(WorldGenRegion pLevel, StructureManager
-// pStructureManager, RandomState pRandom,
-// ChunkAccess pChunk) {
-// // Empty on purpose
+// public void fillFromNoise(WorldGenRegion region, StructureManager
+// structureManager, RandomState randomState,
+// ChunkAccess chunk) {
+// // No terrain noise to fill in a void world
 // }
 
 // @Override
-// public void spawnOriginalMobs(WorldGenRegion pLevel) {
-// // Empty on purpose
+// public int getBaseHeight(int x, int z, Heightmap.Types heightmapType,
+// LevelHeightAccessor level) {
+// return level.getMinBuildHeight();
 // }
 
 // @Override
-// public int getGenDepth() {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'getGenDepth'");
-// }
-
-// @Override
-// public CompletableFuture<ChunkAccess> fillFromNoise(Blender pBlender,
-// RandomState pRandomState,
-// StructureManager pStructureManager, ChunkAccess pChunk) {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'fillFromNoise'");
-// }
-
-// @Override
-// public int getSeaLevel() {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'getSeaLevel'");
-// }
-
-// @Override
-// public int getMinY() {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method 'getMinY'");
-// }
-
-// @Override
-// public int getBaseHeight(int pX, int pZ, Types pType, LevelHeightAccessor
-// pLevel, RandomState pRandom) {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'getBaseHeight'");
-// }
-
-// @Override
-// public NoiseColumn getBaseColumn(int pX, int pZ, LevelHeightAccessor pHeight,
-// RandomState pRandom) {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'getBaseColumn'");
-// }
-
-// @Override
-// public void addDebugScreenInfo(List<String> pInfo, RandomState pRandom,
-// BlockPos pPos) {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'addDebugScreenInfo'");
+// public NoiseColumn getBaseColumn(int x, int z, LevelHeightAccessor level) {
+// return new NoiseColumn(level.getMinBuildHeight(), new BlockState[0]);
 // }
 // }
